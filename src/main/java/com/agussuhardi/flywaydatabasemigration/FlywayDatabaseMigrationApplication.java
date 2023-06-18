8736b450ed2186d44b2b4ece6d4412cafa6e3bd8
agussuhardii <agus.suhardii@gmail.com>
2023-06-18 21:05:20 +0700

    init

---
+++ src/main/java/com/agussuhardi/flywaydatabasemigration/FlywayDatabaseMigrationApplication.java
@@ -0,0 +1,17 @@
+package com.agussuhardi.flywaydatabasemigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author agus.suhardii@gmail.com
+ * @created 18/06/23/06/2023 :21.04
+ * @project flyway-database-migration
+ */
+@SpringBootApplication
+public class FlywayDatabaseMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayDatabaseMigrationApplication.class, args);
+    }
+
+}
